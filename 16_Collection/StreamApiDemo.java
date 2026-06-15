0a71f62d66b4b1577f722393065928c0b703ebf3
DAlgoSculptor <danishnawaz345678@gmail.com>
2026-06-16 00:31:20 +0530

    learned StreamApi

--- 16_Collection/StreamApiDemo.java
+++ 16_Collection/StreamApiDemo.java
@@ -8,16 +8,19 @@ public class StreamApiDemo {
 
         List<Integer> nums = Arrays.asList(10, 22, 9, 5, 7, 4,8);
 
-        int sum = 0;
         for(Integer n : nums){
             if(n % 2 == 0){
-                n = n * 2;
-                sum = sum + n;
+               System.out.print(n + " ");
             }
         }
-        System.out.println(sum);
+        System.out.println();
 
         // System.out.println(nums);
+
+
+        // using Stream Api
+
+        nums.forEach(n -> System.out.println(n));
 
     }
 }

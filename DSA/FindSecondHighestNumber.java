cee624a022286021c6d56ee99144df230af54861
DAlgoSculptor <danishnawaz345678@gmail.com>
2026-06-18 23:57:29 +0530

    Solved Two DSA Problem Today by myself

---
+++ DSA/FindSecondHighestNumber.java
@@ -0,0 +1,28 @@
+public class FindSecondHighestNumber {
+    public static void main(String[] args) {
+        int[] arr = {10, 30, 5, 60, 36};
+
+
+        int max = arr[0];
+
+
+        for(int i = 1; i<arr.length; i++){
+            if(arr[i] >max){
+                max = arr[i];
+            }
+        }
+
+
+        int secondMax = Integer.MIN_VALUE;
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] !=max && arr[i] > secondMax){
+            secondMax = arr[i];
+            }
+        }
+        System.out.println(secondMax);
+
+    }
+    
+
+}
